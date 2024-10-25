d660ea9e08c3416c32bc6a2b47748fb201142484
Baris eren <baris.eren4959@gmai.com>
2024-10-25 23:09:01 +0300

    A

---
+++ HACKATHON/FRONTEND/ROADMAP/roadmap.js
@@ -0,0 +1,9 @@
+// Menü Açma/Kapama
+function toggleMenu() {
+    var navList = document.getElementById("nav-list");
+    if (navList.style.display === "block") {
+        navList.style.display = "none";
+    } else {
+        navList.style.display = "block";
+    }
+}
--- HACKATHON/ROADMAP/roadmap.js
+++
@@ -1,9 +0,0 @@
-// Menü Açma/Kapama
-function toggleMenu() {
-    var navList = document.getElementById("nav-list");
-    if (navList.style.display === "block") {
-        navList.style.display = "none";
-    } else {
-        navList.style.display = "block";
-    }
-}
